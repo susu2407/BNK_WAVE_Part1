51161f19f5853b0f5393e4793f6ad332d30eb75f
DecemberHB <27hyobin@gmail.com>
2025-11-25 16:15:56 +0900

    진짜 찐찐찐찐찐찐찐 마지막 죄송함니다

--- src/main/java/kr/co/wave/repository/member/MemberCardRepository.java
+++
@@ -1,6 +0,0 @@
-package kr.co.wave.repository.member;
-
-import kr.co.wave.entity.card.MemberCard;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberCardRepository extends JpaRepository<MemberCard, Integer> {}
