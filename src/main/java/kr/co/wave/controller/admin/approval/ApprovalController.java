f4da340b912916858bf9c9bace6efc2dba2e3cc1
tndus <tndus159200297@gmail.com>
2025-11-25 12:41:40 +0900

    승인 중

---
+++ src/main/java/kr/co/wave/controller/admin/approval/ApprovalController.java
@@ -0,0 +1,17 @@
+package kr.co.wave.controller.admin.approval;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ApprovalController {
+
+    // 관리자 승인 페이지로 이동
+    @GetMapping("/admin/approval/approval")
+    public String approvalPage () {
+        return "admin/approval/approval";
+    }
+
+}
